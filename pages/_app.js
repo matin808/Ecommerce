17249f94d08a6a8a8cf01679adc6e56ca8b52c19
matin808 <matinkadri78692@gmail.com>
2022-07-04 23:52:34 +0530

    Added Context

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,17 @@
 import "../styles/globals.css";
 import React from "react";
+import { Toaster } from "react-hot-toast"; // for notification pop-up
 import { Layout } from "../components";
+import { StateContext } from "../context/StateContext"; // state
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
   );
 }
 
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { client, urlFor } from "../../lib/client";
 import {
   AiOutlineMinus,
@@ -8,8 +8,16 @@ import {
 } from "react-icons/ai";
 import { Product } from "../../components";
 
+// for state
+import { useStateContext } from "../../context/StateContext";
+
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
+
+  const [index, setIndex] = useState(0);
+
+  //
+  const { decQty, incQty, qty } = useStateContext();
   return (
     <div>
       <div className="product-detail-container">
@@ -17,15 +25,21 @@ const ProductDetails = ({ product, products }) => {
           <div className="image-container">
             <img
               className="product-detail-image"
-              src={urlFor(image && image[0])}
+              src={urlFor(image && image[index])}
             />
           </div>
 
-          {/* <div className="small-images-container ">
+          <div className="small-images-container ">
             {image?.map((item, i) => (
-              <img src={urlFor(item)} className="" onMouseEnter="" />
+              <img
+                src={urlFor(item)}
+                className={
+                  i === index ? "small-image selected-image" : "small-image"
+                }
+                onMouseEnter={() => setIndex(i)}
+              />
             ))}
-          </div> */}
+          </div>
         </div>
 
         <div className="product-detail-desc">
@@ -48,11 +62,11 @@ const ProductDetails = ({ product, products }) => {
           <div className="quantity">
             <h3>Quantity: </h3>
             <p className="quantity-desc">
-              <span className="minus" onClick="">
+              <span className="minus" onClick={decQty}>
                 <AiOutlineMinus />
               </span>
-              <span className="num">0</span>
-              <span className="plus" onClick="">
+              <span className="num">{qty}</span>
+              <span className="plus" onClick={incQty}>
                 <AiOutlinePlus />
               </span>
             </p>
@@ -111,7 +125,7 @@ export const getStaticProps = async ({ params: { slug } }) => {
   const product = await client.fetch(query);
   const products = await client.fetch(productsQuery);
 
-  console.log(product);
+  // console.log(product);
 
   return {
     props: { products, product },
